901c898b81b5338b1a5805eaf442a6ffb9f18667
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-07-19 20:46:18 +0530

    feat(rate limiter): created the rate limiter middleware

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// This is applied beacause while deploying the app will be behind a proxy so in order to know the real IP address of the client this is used.
+app.set('trust proxy', 1);
+
+
 await connectToDb();
 
 app.get("/health", (req, res) => {
